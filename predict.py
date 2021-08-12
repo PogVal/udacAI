9d83484534b99b61894634c4b6181a12157c221d
PogVal <88801888+PogVal@users.noreply.github.com>
2021-08-12 22:23:29 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -19,17 +19,19 @@ import time
 import argparse
 import utilities_2
 
+import os, random
+import tensorflow as tf
 
 
 
 
 args = argparse.ArgumentParser(description='predict.py')
-args.add_argument('--input_img', action="store", type = str, default='flowers/test/28/image_05230.jpg', help='chemin daccès pour les images')
-args.add_argument('--checkpoint', nargs='*', action="store",type = str, default='./checkpoint.pth', help='checkpoint utilisé pour charger le modèle entrainé')
-args.add_argument('--top_k', dest="top_k", action="store", type=int,  default=5, help='renvoie les catégories les plus probables')
-args.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json', help='sse mapping des catégories aux noms réels')
+args.add_argument('--input_img', action="store", type = str, default='flowers/test/28/image_05230.jpg', help='url pour les images')
+args.add_argument('--checkpoint', nargs='*', action="store", type = str, default='./checkpoint.pth', help='checkpoint > charger le modèle entrainé')
+args.add_argument('--top_k', dest="top_k", action="store", type=int,  default=5, help='catégories les plus probables')
+args.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json', help='mapping des catégories')
 args.add_argument('--gpu', action="store_true", dest="gpu", default="False", help='GPU')
-args.add_argument('--arch', dest="arch", action="store", choices=["vgg19","vgg19"], default="vgg19", type = str, help='sets the architecture of the network')
+args.add_argument('--arch', dest="arch", action="store", choices=["densenet121","vgg19"], default="vgg19", type = str, help='type architecture pour network')
 
 
 args = args.parse_args()
@@ -40,68 +42,64 @@ checkpoint = args.checkpoint
 category_names = args.category_names
 arch = args.arch
 
-print("image_path : ", image_path)
+print("url images : ", image_path)
 print("top_k : ", top_k)
 print("gpu : ", gpu)
 print("checkpoint : ", checkpoint)
-print("category_names : ", category_names)
-
-
-'''
-x = image_path.split("/")
-
-with open(category_names, 'r') as f:
-    flower_to_name = json.load(f)
-flower_species=len(flower_to_name)
-target_class=flower_to_name[x[-2]]
-
-idx_to_class = {model.class_to_idx[i]:i for i in model.class_to_idx.keys()}
-classes = [flower_to_name[idx_to_class[c]] for c in kclass]
-data = {'Predicted Class':classes,'Probablity': value}
-dframe = pd.DataFrame(data)
-
-
-print("Image file:", image_path)
-print("Target class:", target_class)
-print(dframe.to_string(index = False))
-'''
+print("nom catégories : ", category_names)
+print("architecture : ", arch)
 
 
 
-
-
+# ---------- F > charger le modèle -----------
 with open('cat_to_name.json', 'r') as f:
     flower_to_name = json.load(f)
-    flower_species = len(flower_to_name)
+
+flower_species = len(flower_to_name)
+path = 'checkpoint.pth'  
+    
+model = utilities_2.load_model(path, flower_species)        
+print("modèle chargé", model)
 
 
-path = 'checkpoint.pth'
-image = ("flowers/test/4/image_05658.jpg")
 
+# ---------- F > traitement des images -----------
+image_path = ("flowers/test/4/image_05658.jpg")
+model = utilities_2.process_image(image_path) # -> conversion
+
+#model_imshow = utilities_2.imshow(image, ax=None, title=None)
+
+
+
+
+
+# ---------- F > prédictions des images -----------
+
+img = random.choice(os.listdir('./flowers/test/40/'))
+img_path = './flowers/test/40/' + img
+
+image = utilities_2.process_image(img_path) # --> renvoie un Tensor
+#print('image_____________', image)
 
 model = utilities_2.load_model(path, flower_species)
-model = utilities_2.process_image(image)
-#model = utilities_2.imshow(image, ax=None, title=None)
+#print('model_____________', model)
 
 
 
-
-img = 'image_04563.jpg'
 path = './flowers/test/40/' + img
-model_2 = models.vgg19(pretrained=True)
 
-idx_to_class = {model_2.class_to_idx[i]:i for i in model_2.class_to_idx.keys()}
-#classes = [flower_to_name[idx_to_class[c]] for c in kclass]
-#data = {'Predicted Class':classes,'Probablity': value}
-#dframe = pd.DataFrame(data)
+prob, classes = utilities_2.predict(img_path, model)
 
 
+print('#######################################################################')
+for i in range(5):
+    print('Flower Class : ', flower_to_name[classes[i]], "// Probability : ", prob[i])
+    print('-----------------------------------------------------')
 
-model = utilities_2.predict(path, model_2, topk=5)
+print('#######################################################################')
 
 
 
-#
 
 
 #
